d1aa9f893fda20d46e2ec88f64d3d271c03e5145
Zual Rana <freelancerzual@gmail.com>
2023-04-08 15:10:34 +0600

    add an events on a btn

--- js/event.js
+++ js/event.js
@@ -1,3 +1,25 @@
 function handleClick() {
   alert("clicked");
 }
+
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+const makeBlue = document.getElementById("blue");
+
+/* makeBlue.addEventListener("click", function (e) {
+  document.body.style.backgroundColor = "blue";
+});
+ */
+
+// option 2
+makeBlue.onclick = blue;
+function blue() {
+  document.body.style.backgroundColor = "blue";
+}
+/* // option 2 another
+makeBlue.onclick = function blue() {
+  document.body.style.backgroundColor = "blue";
+};
+ */
